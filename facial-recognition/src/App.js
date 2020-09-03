107f5c1a43c6948b2630b9f55f848752838d6115
ThisIsKeb <thisiskeb@gmail.com>
2020-09-03 13:17:53 -0400

    firt major update to the files, added in the facial recongition functionalities

--- facial-recognition/src/App.js
+++ facial-recognition/src/App.js
@@ -3,8 +3,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: 'ff01f9d797c54587a32ef6e624f06000'
+});
 
 const particleOptions = {
       "particles": {
@@ -59,6 +65,47 @@ const particleOptions = {
   }
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageURL: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) =>
+  {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+    console.log(box);
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", 
+       this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+
   render(){
   return (
     <div className="App">
@@ -67,9 +114,11 @@ class App extends React.Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/*
-            <FaceRecognition />*/}
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}
+      />
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
   );
  }
